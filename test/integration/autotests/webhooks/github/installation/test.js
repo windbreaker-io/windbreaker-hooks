1870aeef1d82828d2208f8c7e13487d2cab3f965
Druotic <jamesjbeavers@gmail.com>
2017-09-10 19:36:03 -0400

    [WB-25] Add installation event tests

--- test/integration/autotests/webhooks/github/installation/test.js
+++ test/integration/autotests/webhooks/github/installation/test.js
@@ -1,10 +1,10 @@
-exports.description = 'should handle valid push payload'
+exports.description = 'should handle valid installation payload'
 exports.type = 'GITHUB'
 
 exports.request = function ({ request, id, httpServerPort, endpoint, payload, signatureBuilder }) {
   return request
     .post(endpoint)
-    .set('x-github-event', 'push')
+    .set('x-github-event', 'installation')
     .set('x-hub-signature', signatureBuilder(payload))
     .send(payload)
 }
