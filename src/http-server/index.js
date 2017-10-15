519eb074eb60fde9c321c28ee17dec382cd7f26f
austinkelleher <austin.kell47@gmail.com>
2017-10-15 16:39:06 -0400

    [WB-74] Use abstracted version of HTTP logic in util.

--- src/http-server/index.js
+++ src/http-server/index.js
@@ -1,40 +1,19 @@
-const Koa = require('koa')
-// const Router = require('koa-path-router')
-const router = require('koa-router')()
-const getCommonMiddleware = require('~/src/http-server/util/getCommonMiddleware')
+const { createServer } = require('windbreaker-service-util/http')
+const logger = require('~/src/logging').logger(module)
 
 let server
 
+const routes = [
+  '~/src/http-server/routes/healthcheck',
+  '~/src/http-server/routes/github'
+].map((routePath) => require(routePath))
+
 exports.create = function () {
   const config = require('~/src/config')
-  const routes = require('./routes')
-
-  const app = new Koa()
-
-  // TODO: Get koaBody working with koa-path-router
-  // app.use(koaBody)
-  //   .use(ctx => {
-  //     ctx.body = ctx.request.body
-  //   })
-  //
-  // const router = new Router()
-  // routes.register(app, router)
-  //
-  // app.use(router.getRequestHandler())
-
-  getCommonMiddleware().forEach((middleware) => app.use(middleware))
-
-  routes.register(app, router)
-
-  app.use(router.routes())
-  app.use(router.allowedMethods())
-
-  // TODO: Use koa-sslify to add certs
-  server = app.listen(config.getHttpServerPort())
-
-  console.log('Starting on port: ', config.getHttpServerPort())
+  const httpServerPort = config.getHttpServerPort()
+  server = createServer({ httpServerPort, routes, logger })
 }
 
 exports.close = function () {
-  server.close()
+  return server.close()
 }
